e8fb3acf97e073b87d8164e94be24945a7a8ca58
teonsen <straygizmo@gmail.com>
2020-02-05 23:11:55 +0900

    Add OpenCV Activities

---
+++ RPAStudio/Activities/RPA.OpenCV.Activities/FlaxCV.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPA.OpenCV.Activities
+{
+    public class FlaxCV
+    {
+        private int _retryTimes = 600;
+        private int _matchingInterval = 2000;
+        private int _matchingThreshold = 91;
+        private bool _waitExit = true;
+        // you can get Flax.CV.exe here.
+        // https://github.com/teonsen/Flax.CV/releases
+        internal static readonly string _FlaxCV_exe = Environment.CurrentDirectory + @"\Flax.CV.exe";
+
+        internal enum CvActionType : int
+        {
+            DoNothing,
+            LClick,
+            DoubleClick,
+            RightClick,
+            Move,
+            Matching,
+            CompareHist
+        }
+
+        public int RetryTimes {
+            get { return _retryTimes; }
+            set { _retryTimes = value; }
+        }
+
+        public int MatchingInterval {
+            set {
+                if (value < 300)
+                {
+                    _matchingInterval = 300;
+                }
+                else
+                {
+                    _matchingInterval = value;
+                }
+            }
+            get {
+                return _matchingInterval;
+            }
+        }
+
+        public int MatchingThreshold {
+            set {
+                if (value < 80)
+                {
+                    _matchingThreshold = 80;
+                }
+                else if (value > 100)
+                {
+                    _matchingThreshold = 100;
+                }
+                else
+                {
+                    _matchingThreshold = value;
+                }
+            }
+            get {
+                return _matchingThreshold;
+            }
+        }
+
+        public bool WaitExit {
+            get { return _waitExit; }
+            set { _waitExit = value; }
+        }
+
+        private Rectangle Rect0 {
+            get { return new Rectangle(0, 0, 0, 0); }
+        }
+
+        public void Click(string templateImagePath, int retryTimes = 600)
+        {
+            ActionOnImage(CvActionType.LClick, templateImagePath, retryTimes, Rect0);
+        }
+
+        public void DoubleClick(string templateImagePath, int retryTimes = 600)
+        {
+            ActionOnImage(CvActionType.DoubleClick, templateImagePath, retryTimes, Rect0);
+        }
+
+        public void RightClick(string templateImagePath, int retryTimes = 600)
+        {
+            ActionOnImage(CvActionType.RightClick, templateImagePath, retryTimes, Rect0);
+        }
+
+        public void Hover(string templateImagePath, int retryTimes = 600)
+        {
+            ActionOnImage(CvActionType.Move, templateImagePath, retryTimes, Rect0);
+        }
+
+        public void Wait(string templateImagePath, int retryTimes = 600)
+        {
+            ActionOnImage(CvActionType.DoNothing, templateImagePath, retryTimes, Rect0);
+        }
+
+        internal CvResult ActionOnImage(CvActionType actionType, string templateImagePath, int retryTimes, Rectangle rect, string sourceImagePath = "")
+        {
+            retryTimes = retryTimes < 0 ? _retryTimes : retryTimes;
+            return DoCVAction(actionType, templateImagePath, _matchingThreshold, _matchingInterval, retryTimes, rect, sourceImagePath);
+        }
+
+        internal CvResult DoCVAction(CvActionType actionType,
+                                        string templateImagePath,
+                                        int matchingThreshold,
+                                        int matchingInterval,
+                                        int retry,
+                                        Rectangle rect,
+                                        string sourceImagePath = "")
+        {
+            if (File.Exists(templateImagePath))
+            {
+                var psi = new ProcessStartInfo();
+                psi.FileName = _FlaxCV_exe;
+                psi.WindowStyle = ProcessWindowStyle.Normal;
+                psi.UseShellExecute = false;
+                int minimizeCucumber = 0;
+                int width = rect.Width;
+                int height = rect.Height;
+                var psb = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+                if (width <= 0 || height <= 0)
+                {
+                    // Get full screen capture if capture width <= 0.
+                    width = psb.Width;
+                    height = psb.Height;
+                }
+                if (rect.X >= psb.Width - 300 && rect.Y >= psb.Height - 250)
+                {
+                    // Minimize Flax.CV.exe if search area might overlaps on it. 
+                    minimizeCucumber = -1;
+                }
+                int a = (int)actionType;
+                string sArgs = string.Format(@"{0} ""{1}"" {2} {3} {4} {5} {6} {7} {8} {9} ""{10}""",
+                                            (int)actionType,       // Command
+                                            templateImagePath,     // Template image path
+                                            matchingThreshold,     // Matching threshold
+                                            matchingInterval,      // Matching interval
+                                            retry,                 // Number of retry
+                                            minimizeCucumber,
+                                            rect.X,                // Capture start point x
+                                            rect.Y,                // Capture start point y
+                                            width,                 // Capture width
+                                            height,                // Capture height
+                                            sourceImagePath);      // Compared image path
+                psi.Arguments = sArgs;
+                var p = Process.Start(psi);
+
+                int matchedLevel = 0;
+                int posX = -1, posY = -1;
+                if (_waitExit)
+                {
+                    p.WaitForExit();
+                    int exitCode = p.ExitCode;
+                    if (exitCode >= 0)
+                    {
+                        // Get 25 to 31bit value
+                        matchedLevel = exitCode >> 24;
+                        // Get 13 to 24bit value + offset x = detected x cordinate
+                        posX = ((exitCode >> 12) & 0xFFF) + rect.X;
+                        // Get 1 to 12bit value + offset y = detected y cordinate
+                        posY = (exitCode & 0xFFF) + rect.Y;
+                    }
+                    else
+                    {
+                        string cuexeName = Path.GetFileNameWithoutExtension(_FlaxCV_exe);
+                        if (exitCode == -4)
+                        {
+                            //SysLog.MyWarning(string.Format("{0} : Timeout", cuexeName));
+                        }
+                    }
+                }
+                //SysLog.DebugPrint(String.Format("Matched={0} at ({1},{2})", iMatched.ToString(), iPosX.ToString(), iPosY.ToString()));
+                p.Close();
+                p.Dispose();
+                bool isMatched = matchedLevel >= _matchingThreshold;
+                return new CvResult(matchedLevel, posX, posY, isMatched);
+            }
+            //SysLog.MyError("IfFileExistsThenDoAction", "Template file does not exist. " + templateFileFullPath);
+            return new CvResult();
+        }
+
+        public class CvResult
+        {
+            public bool IsMatched;
+            public int MatchedLevel;
+            public Point Pt;
+
+            public CvResult(int matchedLev = 0, int posX = -1, int posY = -1, bool isMatched = false)
+            {
+                MatchedLevel = matchedLev;
+                Pt = new Point(posX, posY);
+                IsMatched = isMatched;
+            }
+
+            // http://stackoverflow.com/questions/21485644/implicit-bool-and-operator-override-handle-if-statements-correctly
+            public static implicit operator bool(CvResult cr)
+            {
+                return !ReferenceEquals(cr, null) && cr.IsMatched;
+            }
+        }
+
+    }
+}
---
+++ RPAStudio/Activities/RPA.OpenCV.Activities/Mouse/CVClickActivity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using Plugins.Shared.Library;
+
+namespace RPA.OpenCV.Activities.Mouse
+{
+    [Designer(typeof(CVClickActivityDesigner))]
+    public sealed class CVClickActivity : CodeActivity
+    {
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        public CVClickActivity()
+        {
+            //File.Delete(FlaxCV._FlaxCV_exe);
+            using (FileStream fs = new FileStream(FlaxCV._FlaxCV_exe, FileMode.Create))
+            {
+                fs.Write(Properties.Resources.Flax_CV, 0, Properties.Resources.Flax_CV.Length);
+            }
+        }
+
+        public new string DisplayName;
+        [Browsable(false)]
+        public string _DisplayName { get { return "CV Click"; } }
+
+        [Browsable(false)]
+        public string SourceImgPath { get; set; }
+
+        [Browsable(false)]
+        public string icoPath {
+            get {
+                return @"pack://application:,,,/RPA.OpenCV.Activities;Component/Resources/Mouse/cvclick.png";
+            }
+        }
+
+        [Category("Public")] //公共 //Public //一般公開
+        [DisplayName("Error execution")] //错误执行 //Error execution //エラー実行
+        [Description("Specifies whether automation should continue even if the activity raises an error")] //指定即使活动引发错误，自动化是否仍应继续 //Specifies whether automation should continue even if the activity raises an error //アクティビティでエラーが発生した場合でも自動化を続行するかどうかを指定します
+        public InArgument<bool> ContinueOnError { get; set; }
+
+        [Category("Input")] //输入 //Input //入力
+        [Browsable(true)]
+        [DisplayName("Window Title")] //窗口标题 //Window title //ウィンドウタイトル
+        [Description("Enter the Window Title")] //输入窗口标题 //Enter window title //Windowのタイトルを入力
+        public InArgument<string> Title {
+            get;
+            set;
+        }
+
+        [Category("入力")]
+        [Browsable(true)]
+        [DisplayName("MatchingThreshold")]
+        [Description("マッチング閾値 (デフォルト値 = 91. Must 80 <= x <= 100)")]
+        public InArgument<int> MatchingThreshold { get; set; } = 91;
+
+        [Category("入力")]
+        [Browsable(true)]
+        [DisplayName("MatchingInterval")]
+        [Description("マッチング間隔[ms] (デフォルト値 = 2000[ms]. Must 600 <= x)")]
+        public InArgument<int> MatchingInterval { get; set; } = 2000;
+
+        [Category("入力")]
+        [Browsable(true)]
+        [DisplayName("Retry")]
+        [Description("リトライ回数 (デフォルト値 = 600回)")]
+        public InArgument<int> Retry { get; set; } = 600;
+
+        [Category("出力")]
+        [Browsable(true)]
+        public OutArgument<bool> Result { get; set; }
+
+        private System.Windows.Visibility visi = System.Windows.Visibility.Hidden;
+        [Browsable(false)]
+        public System.Windows.Visibility visibility {
+            get {
+                return visi;
+            }
+            set {
+                visi = value;
+            }
+        }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            int x = 0, y = 0;
+            // Disply Width
+            int w = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            // Disply Height
+            int h = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+
+            try
+            {
+                string title = Title.Get(context);
+                if (title != null && title.Length > 0)
+                {
+                    IntPtr hWnd = IntPtr.Zero;
+                    hWnd = Win32Api.FindWindow(null, title);
+                    RECT rect;
+                    bool flag = GetWindowRect(hWnd, out rect);
+                    x = rect.left;
+                    y = rect.top;
+                    w = rect.right - rect.left;
+                    h = rect.bottom - rect.top;
+                }
+                var cv = new FlaxCV();
+                int matchingThreshold = MatchingThreshold.Get(context);
+                int matchingInterval = MatchingInterval.Get(context);
+                int retry = Retry.Get(context);
+                var cvRet = cv.DoCVAction(FlaxCV.CvActionType.LClick, SourceImgPath, matchingThreshold, matchingInterval, retry, new System.Drawing.Rectangle(x, y, w, h));
+                string resultIfo = string.Format("Capture Area : x={0}, y={1}, w={2}, h={3}\nMatched = {4}\nMatched Level = {5}", x, y, w, h, cvRet.IsMatched, cvRet.MatchedLevel);
+                SharedObject.Instance.Output(SharedObject.enOutputType.Information, "Image Matching Result", resultIfo);
+                Result.Set(context, cvRet.IsMatched);
+            }
+            catch (Exception e)
+            {
+                SharedObject.Instance.Output(SharedObject.enOutputType.Error, "Error on Executing CVClickActivity()", e.Message);
+                if (ContinueOnError.Get(context))
+                {
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
