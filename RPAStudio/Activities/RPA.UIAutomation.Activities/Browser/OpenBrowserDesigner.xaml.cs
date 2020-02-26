7820f92ef6bf01e39f18feb37b3d707d55756980
DESKTOP-G17U1FM\HELLO <miaoguangyu@pansoft.com>
2020-02-26 12:04:50 +0800

    remove litjson

--- RPAStudio/Activities/RPA.UIAutomation.Activities/Browser/OpenBrowserDesigner.xaml.cs
+++ RPAStudio/Activities/RPA.UIAutomation.Activities/Browser/OpenBrowserDesigner.xaml.cs
@@ -1,26 +1,11 @@
-﻿using LitJson;
-using Plugins.Shared.Library;
-using System;
+﻿using System;
 using System.Activities.Presentation.Model;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using RPA.UIAutomation.Activities.Mouse;
 using System.Runtime.InteropServices;
-using System.Activities;
+
 
 namespace RPA.UIAutomation.Activities.Browser
 {
